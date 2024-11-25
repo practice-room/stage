2590f23fc68e588b4eaab22bbd92bce49333af6d
komish-impersonator <72107629+komish-impersonator@users.noreply.github.com>
2024-11-25 11:57:08 -0600

    Release-1.99.99 (#4)
    Co-authored-by: komish-impersonator <41898282+github-actions[bot]@users.noreply.github.com>

--- tests/functional/behave_features/common/utils/env.py
+++ tests/functional/behave_features/common/utils/env.py
@@ -5,7 +5,7 @@ import os
 
 from common.utils.setttings import *
 
-
+# (jose): this can stay
 def get_bot_name_and_token():
     bot_name = os.environ.get("BOT_NAME")
     logging.debug(f"Enviroment variable value BOT_NAME: {bot_name}")
@@ -21,47 +21,50 @@ def get_bot_name_and_token():
         raise Exception("BOT_NAME set but BOT_TOKEN not specified")
     return bot_name, bot_token
 
+ 
+# (jose) mark for deletion
+# def get_dry_run():
+#     # Accepts 'true' or 'false', depending on whether we want to notify
+#     # Don't notify on dry runs, default to True
+#     dry_run = False if os.environ.get("DRY_RUN") == "false" else True
+#     # Don't notify if not triggerd on PROD_REPO and PROD_BRANCH
+#     if not dry_run:
+#         triggered_branch = os.environ.get("GITHUB_REF").split("/")[-1]
+#         triggered_repo = os.environ.get("GITHUB_REPOSITORY")
+#         if triggered_repo != PROD_REPO or triggered_branch != PROD_BRANCH:
+#             dry_run = True
+#     return dry_run
 
-def get_dry_run():
-    # Accepts 'true' or 'false', depending on whether we want to notify
-    # Don't notify on dry runs, default to True
-    dry_run = False if os.environ.get("DRY_RUN") == "false" else True
-    # Don't notify if not triggerd on PROD_REPO and PROD_BRANCH
-    if not dry_run:
-        triggered_branch = os.environ.get("GITHUB_REF").split("/")[-1]
-        triggered_repo = os.environ.get("GITHUB_REPOSITORY")
-        if triggered_repo != PROD_REPO or triggered_branch != PROD_BRANCH:
-            dry_run = True
-    return dry_run
-
-
-def get_notify_id():
-    # Accepts comma separated Github IDs or empty strings to override people to tag in notifications
-    notify_id = os.environ.get("NOTIFY_ID")
-    if notify_id:
-        notify_id = [vt.strip() for vt in notify_id.split(",")]
-    else:
-        notify_id = ["dperaza", "mmulholla"]
-    return notify_id
-
-
-def get_software_name_version():
-    software_name = os.environ.get("SOFTWARE_NAME")
-    if not software_name:
-        raise Exception("SOFTWARE_NAME environment variable not defined")
-
-    software_version = os.environ.get("SOFTWARE_VERSION").strip('"')
-    if not software_version:
-        raise Exception("SOFTWARE_VERSION environment variable not defined")
-    elif software_version.startswith("sha256"):
-        software_version = software_version[-8:]
-
-    return software_name, software_version
+# (jose) mark for deletion
+# def get_notify_id():
+#     # Accepts comma separated Github IDs or empty strings to override people to tag in notifications
+#     notify_id = os.environ.get("NOTIFY_ID")
+#     if notify_id:
+#         notify_id = [vt.strip() for vt in notify_id.split(",")]
+#     else:
+#         notify_id = ["dperaza", "mmulholla"]
+#     return notify_id
 
 
-def get_vendor_type():
-    vendor_type = os.environ.get("VENDOR_TYPE")
-    if not vendor_type:
-        logging.info("VENDOR_TYPE environment variable not defined, default to `all`")
-        vendor_type = "all"
-    return vendor_type
+# (jose) mark for deletion
+# def get_software_name_version():
+#     software_name = os.environ.get("SOFTWARE_NAME")
+#     if not software_name:
+#         raise Exception("SOFTWARE_NAME environment variable not defined")
+
+#     software_version = os.environ.get("SOFTWARE_VERSION").strip('"')
+#     if not software_version:
+#         raise Exception("SOFTWARE_VERSION environment variable not defined")
+#     elif software_version.startswith("sha256"):
+#         software_version = software_version[-8:]
+
+#     return software_name, software_version
+
+# (jose) mark for deletion. note that there is another function with the same
+# name that should remain and will continue to be used.
+# def get_vendor_type():
+#     vendor_type = os.environ.get("VENDOR_TYPE")
+#     if not vendor_type:
+#         logging.info("VENDOR_TYPE environment variable not defined, default to `all`")
+#         vendor_type = "all"
+#     return vendor_type
